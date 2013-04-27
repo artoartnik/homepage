040fed70f3fd54f7e0ff6cdd42d8c06800dce075
Arto <arto@arto-pc.(none)>
2013-04-27 21:07:51 +0200

    Added new post and edit post features.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.all
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	Post.create params[:post]
+  	redirect_to root_path
+  end
+
+  def edit
+  	@post = Post.find params[:id]
+  end
+
+  def update
+  	post = Post.find params[:id]
+  	if post.update_attributes params[:post]
+  		redirect_to root_path
+  	end
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,45 @@
 require 'spec_helper'
 
 describe "Posts" do
+
+  before do
+	@post = Post.create :title => 'New post', :content => 'Some text for the post.'
+  end 
+  
   describe "GET /posts" do
     it "displays some posts" do
     	visit posts_path
     	page.should have_content "New post"
     end
+
+    it "creates a new post" do
+    	visit new_post_path
+    	
+    	fill_in 'Title', :with => 'Newly created post'
+    	fill_in 'Content', :with => 'New post content.'
+
+    	click_button 'Create Post'
+
+    	current_path.should == root_path
+    	page.should have_content 'New post content.'
+    end
+  end
+
+  describe "PUT /posts" do
+  	it "edits a post" do
+  		
+  		visit posts_path
+  		click_link 'Edit'
+
+  		current_path.should == edit_post_path(@post)
+  		page.should have_content 'Some text for the post.'
+  		
+  		fill_in 'Content', :with => 'Edited text of a post.'
+  		click_button 'Update Post'
+
+  		current_path.should == root_path
+  		page.should have_content 'Edited text of a post.'
+
+  	end
   end
 end
